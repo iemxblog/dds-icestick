15e46b278bfea7c0bc0fdc6fd51b99159e6a3e7b
iemxblog <iemxblog@gmail.com>
2015-09-04 22:08:53 +0200

    Rewrote communication module

--- communication.v
+++ communication.v
@@ -9,72 +9,88 @@ module communication(
 
 	output reg en=0,
 	output wire [31:0] m,
-	output reg set=0	
+	output reg set=0
 
 );
 
-reg [7:0] state=`COMMAND;
+reg [7:0] state=0;
 reg [7:0] command=0;
 reg [7:0] m0=0, m1=0, m2=0, m3=0;
-reg gen_pulse=0;
 
 assign m = {m3, m2, m1, m0};
 
 
 // state machine to receive data from the UART
-always @ (posedge received)
+always @ (posedge clk)
 begin
 	case(state)
-		`COMMAND: begin
-				command = rx_byte;
-				case(command)
-					`BYTE0: state = `DATA;
-					`BYTE1: state = `DATA;
-					`BYTE2: state = `DATA;
-					`BYTE3: state = `DATA;
-					`ENABLE: begin
-							en = 1; 
-							state = `COMMAND;
+		0: begin
+			if (received == 1) begin
+				case(rx_byte)
+					`BYTE0:	begin
+							command = rx_byte;
+							state=1;
 						end
-					`DISABLE: begin
-							en = 0; 
-							state = `COMMAND;
+					`BYTE1:	begin
+							command = rx_byte;
+							state=1;
 						end
-					`SET: 	begin
-							gen_pulse=1;
-							state = `COMMAND;
+					`BYTE2: begin
+							command = rx_byte;
+							state=1;
 						end
-					default: state = `COMMAND;
+					`BYTE3:	begin
+							command = rx_byte;
+							state=1;
+						end
+					`ENABLE: state = 2;
+					`DISABLE: state = 3;
+					`SET: state = 4;
+					default: state = 8;
 				endcase
 			end
-		`DATA: 	begin	
+		end
+		1: begin
+			if (received == 1)
+			begin
 				case(command)
 					`BYTE0: m0 = rx_byte;
 					`BYTE1: m1 = rx_byte;
 					`BYTE2: m2 = rx_byte;
 					`BYTE3: m3 = rx_byte;
 				endcase
-				state = `COMMAND;
+				state=6;
 			end
+		end
+		2: begin
+			en=1;
+			state=6;
+		end
+		3: begin
+			en=0;
+			state=6;
+		end
+		4: begin
+			set=1;
+			state=5;
+		end
+		5: begin
+			set=0;
+			state=6;
+		end
+		6: begin
+			tx_byte=`ACK;
+			transmit=1;
+			state=7;
+		end
+		7: begin
+			transmit=0;
+			state=0;
+		end
+		8: begin // error state
+			state=0;
+		end
 	endcase
 end
-
-// pulse generator for the 'set' output
-// to generate a pulse, the 'gen_pulse' reg is set to 1
-// and the following code generates the pulse during one clock cycle
-always @ (posedge clk)
-begin
-	if (gen_pulse==1)
-	begin
-		gen_pulse=0;
-		set=1;
-	end
-end
-
-always @ (negedge clk) begin
-	set=0;
-end
-
-// end of pulse generator
 
 endmodule
